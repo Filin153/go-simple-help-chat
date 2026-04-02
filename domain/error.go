6a4be7bbe510c189d1994b2e9fcf522e71254850
Dmitry Savchenko <dsavchenko@titan-it.pro>
2026-04-02 11:54:30 +0300

    update Schedule

--- domain/error.go
+++ domain/error.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrInvalidTimeFormat error = errors.New("Параметры from и to имеют неверный формат (пример: 08:00, 18:00)")
+	ErrInvalidDayType    error = errors.New("Значение True может стоять только у is_break или тольок у is_week_end")
 	ErrLogin             error = errors.New("Логи или пароль некорректны")
 	ErrShortPassword     error = errors.New("Пароль должен быть равен или длинее 6 символов")
 )
--- domain/schedule.go
+++ domain/schedule.go
@@ -1,52 +1,30 @@
 package domain
 
 import (
-	"strings"
-)
-
-type ScheduleType string
-
-const (
-	ScheduleTypeDay   ScheduleType = "day"
-	ScheduleTypeBreak ScheduleType = "break"
+	"time"
 )
 
 type CreateScheduleBreak struct {
-	Name  string `json:"name" validate:"required,max=10"`
-	From  string `json:"from" validate:"required"`
-	To    string `json:"to" validate:"required"`
-	DayID int    `json:"day_id,omitempty" validate:"required"`
+	Name      string    `json:"name" validate:"required,max=10"`
+	From      time.Time `json:"from" validate:"required"`
+	To        time.Time `json:"to" validate:"required"`
+	IsBreak   bool      `json:"is_break"`
+	IsWeekEnd bool      `json:"is_week_end"`
 }
 
-func (c *CreateScheduleBreak) ValidateTime() error {
-	if !strings.Contains(c.From, ":") || !strings.Contains(c.To, ":") {
-		return ErrInvalidTimeFormat
+func (c *CreateScheduleBreak) Validate() error {
+	if c.IsBreak && c.IsWeekEnd {
+		return ErrInvalidDayType
 	}
-
-	fromSplit := strings.Split(c.From, ":")
-	toSplit := strings.Split(c.To, ":")
-
-	if len(fromSplit) != 2 {
-		return ErrInvalidTimeFormat
-	} else if len(fromSplit[0]) != 2 || len(fromSplit[1]) != 2 {
-		return ErrInvalidTimeFormat
-	} else if len(toSplit) != 2 {
-		return ErrInvalidTimeFormat
-	} else if len(toSplit[0]) != 2 || len(toSplit[1]) != 2 {
-		return ErrInvalidTimeFormat
-	}
-
 	return nil
 }
 
 type Schedule struct {
-	ID           int          `db:"id" json:"id"`
-	DepartmentID int          `db:"department_id" json:"department_id"`
-	Name         string       `db:"name" json:"name"`
-	From         string       `db:"from" json:"from"`
-	To           string       `db:"to" json:"to"`
-	Type         ScheduleType `db:"type" json:"type"`
-	DayID        int          `db:"day_id" json:"day_id,omitempty"`
-	IsDayOff     bool         `db:"is_day_off" json:"is_day_off"`
-	Breaks       []Schedule   `db:"-" json:"breaks,omitempty"`
+	ID           int       `db:"id" json:"id"`
+	DepartmentID int       `db:"department_id" json:"department_id"`
+	Name         string    `db:"name" json:"name"`
+	From         time.Time `db:"from" json:"from"`
+	To           time.Time `db:"to" json:"to"`
+	IsBreak      bool      `db:"is_break" json:"is_break"`
+	IsWeekEnd    bool      `db:"is_week_end" json:"is_week_end"`
 }
